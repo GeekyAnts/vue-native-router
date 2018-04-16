11e7dcc47c6c25cf001d8018fd80f9c2cec0674a
gauravprwl14 <gauravprwl14@gmail.com>
2018-04-16 13:16:43 +0530

    exporting all the react-navigation properties

--- src/index.js
+++ src/index.js
@@ -82,3 +82,42 @@ export default {
   withOrientation,
   RouterPlugin
 };
+
+export const createNavigationContainer = createNavigationContainer;
+export const StateUtils = StateUtils;
+export const createNavigator = createNavigator;
+export const createStackNavigator = createStackNavigator;
+export const StackNavigator = StackNavigator;
+export const createSwitchNavigator = createSwitchNavigator;
+export const SwitchNavigator = SwitchNavigator;
+export const createDrawerNavigator = createDrawerNavigator;
+export const DrawerNavigator = DrawerNavigator;
+export const createTabNavigator = createTabNavigator;
+export const TabNavigator = TabNavigator;
+export const createBottomTabNavigator = createBottomTabNavigator;
+export const createMaterialBottomTabNavigator = createMaterialBottomTabNavigator;
+export const createMaterialTopTabNavigator = createMaterialTopTabNavigator;
+export const NavigationActions = NavigationActions;
+export const StackActions = StackActions;
+export const DrawerActions = DrawerActions;
+export const StackRouter = StackRouter;
+export const TabRouter = TabRouter;
+export const SwitchRouter = SwitchRouter;
+export const Transitioner = Transitioner;
+export const StackView = StackView;
+export const StackViewCard = StackViewCard;
+export const SafeAreaView = SafeAreaView;
+export const SceneView = SceneView;
+export const ResourceSavingSceneView = ResourceSavingSceneView;
+export const Header = Header;
+export const HeaderTitle = HeaderTitle;
+export const HeaderBackButton = HeaderBackButton;
+export const DrawerView = DrawerView;
+export const DrawerItems = DrawerItems;
+export const TabView = TabView;
+export const TabBarTop = TabBarTop;
+export const TabBarBottom = TabBarBottom;
+export const SwitchView = SwitchView;
+export const withNavigation = withNavigation;
+export const withNavigationFocus = withNavigationFocus;
+export const RouterPlugin = RouterPlugin;
