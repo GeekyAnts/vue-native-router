c4e49e8c2b0d4c78f1abaa74649bf45dfb383e03
Andrew Li <12980971+Li357@users.noreply.github.com>
2018-06-19 17:26:20 -0500

    Keep formatting consistent

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ export default {
   ...ReactNavigation,
   RouterPlugin: RouterPluginDefault
 };
-export * from 'react-navigation'
-export { default } from './routerPlugin';
+export * from "react-navigation";
+export { default } from "./routerPlugin";
