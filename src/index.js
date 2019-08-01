8d5155768eca34f1b078d923c48b9a82e518323e
Rishabh Karnad <rishabhk@geekyants.com>
2019-08-01 12:55:38 +0530

    Upgraded to react-navigation v3

--- src/index.js
+++ src/index.js
@@ -1,89 +1,144 @@
-import ReactNavigation from "react-navigation";
+import {
+  StateUtils,
+  getNavigation,
 
-import RouterPluginDefault from "./routerPlugin";
+  // Navigators
+  createNavigator,
+  NavigationContext,
+  NavigationProvider,
+  NavigationConsumer,
+  createSwitchNavigator,
 
-export default {
-  createNavigationContainer: ReactNavigation.createNavigationContainer,
-  StateUtils: ReactNavigation.StateUtils,
-  createNavigator: ReactNavigation.createNavigator,
-  createStackNavigator: ReactNavigation.createStackNavigator,
-  StackNavigator: ReactNavigation.StackNavigator,
-  createSwitchNavigator: ReactNavigation.createSwitchNavigator,
-  SwitchNavigator: ReactNavigation.SwitchNavigator,
-  createDrawerNavigator: ReactNavigation.createDrawerNavigator,
-  DrawerNavigator: ReactNavigation.DrawerNavigator,
-  createTabNavigator: ReactNavigation.createTabNavigator,
-  TabNavigator: ReactNavigation.TabNavigator,
-  createBottomTabNavigator: ReactNavigation.createBottomTabNavigator,
-  createMaterialBottomTabNavigator:
-    ReactNavigation.createMaterialBottomTabNavigator,
-  createMaterialTopTabNavigator: ReactNavigation.createMaterialTopTabNavigator,
-  NavigationActions: ReactNavigation.NavigationActions,
-  StackActions: ReactNavigation.StackActions,
-  DrawerActions: ReactNavigation.DrawerActions,
-  StackRouter: ReactNavigation.StackRouter,
-  TabRouter: ReactNavigation.TabRouter,
-  SwitchRouter: ReactNavigation.SwitchRouter,
-  Transitioner: ReactNavigation.Transitioner,
-  StackView: ReactNavigation.StackView,
-  StackViewCard: ReactNavigation.StackViewCard,
-  SafeAreaView: ReactNavigation.SafeAreaView,
-  SceneView: ReactNavigation.SceneView,
-  ResourceSavingSceneView: ReactNavigation.ResourceSavingSceneView,
-  Header: ReactNavigation.Header,
-  HeaderTitle: ReactNavigation.HeaderTitle,
-  HeaderBackButton: ReactNavigation.HeaderBackButton,
-  DrawerView: ReactNavigation.DrawerView,
-  DrawerItems: ReactNavigation.DrawerItems,
-  TabView: ReactNavigation.TabView,
-  TabBarTop: ReactNavigation.TabBarTop,
-  TabBarBottom: ReactNavigation.TabBarBottom,
-  SwitchView: ReactNavigation.SwitchView,
-  withNavigation: ReactNavigation.withNavigation,
-  withNavigationFocus: ReactNavigation.withNavigationFocus,
-  withOrientation: ReactNavigation.withOrientation,
-  RouterPlugin: RouterPluginDefault
+  // Actions
+  NavigationActions,
+  StackActions,
+  SwitchActions,
+
+  // Routers
+  StackRouter,
+  TabRouter,
+  SwitchRouter,
+  createConfigGetter,
+  getScreenForRouteName,
+  validateRouteConfigMap,
+
+  // Utils
+  getActiveChildNavigationOptions,
+  pathUtils,
+  SceneView,
+
+  // SwitchView
+  SwitchView,
+
+  // NavigationEvents
+  NavigationEvents,
+
+  // HOCs
+  withNavigation,
+  withNavigationFocus,
+} from '@react-navigation/core';
+
+import {
+  createAppContainer,
+  createKeyboardAwareNavigator,
+  createNavigationAwareScrollable,
+  withOrientation,
+  ResourceSavingSceneView,
+  SafeAreaView,
+} from '@react-navigation/native';
+
+import {
+  createStackNavigator,
+  Transitioner,
+  Assets,
+  Header,
+  HeaderTitle,
+  HeaderBackButton,
+  StackGestureContext,
+  StackView,
+  StackViewCard,
+} from 'react-navigation-stack';
+
+import {
+  createDrawerNavigator,
+  DrawerActions,
+  DrawerView,
+  DrawerNavigatorItems,
+} from 'react-navigation-drawer';
+
+import {
+  createTabNavigator,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  BottomTabBar,
+  MaterialTopTabBar,
+} from 'react-navigation-tabs';
+
+import {
+  createMaterialBottomTabNavigator,
+} from 'react-navigation-material-bottom-tabs'
+
+import RouterPlugin from "./routerPlugin";
+
+export {
+  // core
+  StateUtils,
+  getNavigation,
+  createNavigator,
+  NavigationContext,
+  NavigationProvider,
+  NavigationConsumer,
+  createSwitchNavigator,
+  NavigationActions,
+  StackActions,
+  SwitchActions,
+  StackRouter,
+  TabRouter,
+  SwitchRouter,
+  createConfigGetter,
+  getScreenForRouteName,
+  validateRouteConfigMap,
+  getActiveChildNavigationOptions,
+  pathUtils,
+  SceneView,
+  SwitchView,
+  NavigationEvents,
+  withNavigation,
+  withNavigationFocus,
+
+  // native
+  createAppContainer,
+  createKeyboardAwareNavigator,
+  createNavigationAwareScrollable,
+  withOrientation,
+  ResourceSavingSceneView,
+  SafeAreaView,
+
+  // stack
+  createStackNavigator,
+  Transitioner,
+  Assets,
+  Header,
+  HeaderTitle,
+  HeaderBackButton,
+  StackGestureContext,
+  StackView,
+  StackViewCard,
+
+  // drawer
+  createDrawerNavigator,
+  DrawerActions,
+  DrawerView,
+  DrawerItems,
+
+  // tabs
+  createTabNavigator,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createMaterialBottomTabNavigator,
+  BottomTabBar,
+  MaterialTopTabBar,
+
+  // plugin
+  RouterPlugin,
 };
-
-export const createNavigationContainer =
-  ReactNavigation.createNavigationContainer;
-export const StateUtils = ReactNavigation.StateUtils;
-export const createNavigator = ReactNavigation.createNavigator;
-export const createStackNavigator = ReactNavigation.createStackNavigator;
-export const StackNavigator = ReactNavigation.StackNavigator;
-export const createSwitchNavigator = ReactNavigation.createSwitchNavigator;
-export const SwitchNavigator = ReactNavigation.SwitchNavigator;
-export const createDrawerNavigator = ReactNavigation.createDrawerNavigator;
-export const DrawerNavigator = ReactNavigation.DrawerNavigator;
-export const createTabNavigator = ReactNavigation.createTabNavigator;
-export const TabNavigator = ReactNavigation.TabNavigator;
-export const createBottomTabNavigator =
-  ReactNavigation.createBottomTabNavigator;
-export const createMaterialBottomTabNavigator =
-  ReactNavigation.createMaterialBottomTabNavigator;
-export const createMaterialTopTabNavigator =
-  ReactNavigation.createMaterialTopTabNavigator;
-export const NavigationActions = ReactNavigation.NavigationActions;
-export const StackActions = ReactNavigation.StackActions;
-export const DrawerActions = ReactNavigation.DrawerActions;
-export const StackRouter = ReactNavigation.StackRouter;
-export const TabRouter = ReactNavigation.TabRouter;
-export const SwitchRouter = ReactNavigation.SwitchRouter;
-export const Transitioner = ReactNavigation.Transitioner;
-export const StackView = ReactNavigation.StackView;
-export const StackViewCard = ReactNavigation.StackViewCard;
-export const SafeAreaView = ReactNavigation.SafeAreaView;
-export const SceneView = ReactNavigation.SceneView;
-export const ResourceSavingSceneView = ReactNavigation.ResourceSavingSceneView;
-export const Header = ReactNavigation.Header;
-export const HeaderTitle = ReactNavigation.HeaderTitle;
-export const HeaderBackButton = ReactNavigation.HeaderBackButton;
-export const DrawerView = ReactNavigation.DrawerView;
-export const DrawerItems = ReactNavigation.DrawerItems;
-export const TabView = ReactNavigation.TabView;
-export const TabBarTop = ReactNavigation.TabBarTop;
-export const TabBarBottom = ReactNavigation.TabBarBottom;
-export const SwitchView = ReactNavigation.SwitchView;
-export const withNavigation = ReactNavigation.withNavigation;
-export const withNavigationFocus = ReactNavigation.withNavigationFocus;
-export const RouterPlugin = RouterPluginDefault;
