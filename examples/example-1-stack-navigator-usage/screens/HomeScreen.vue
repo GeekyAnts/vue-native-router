d6be63c187069a741d077cc89171b0f24268dc9e
Rishabh Karnad <rishabhk@geekyants.com>
2019-08-21 16:51:54 +0530

    Created example project with StackNavigator usage

---
+++ examples/example-1-stack-navigator-usage/screens/HomeScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="container">
+    <text>This is the Home screen</text>
+    <button title="Go to the Details screen" @press="changeRoute"></button>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: { type: Object }
+  },
+  methods: {
+    changeRoute() {
+      this.navigation.navigate('Details');
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+</style>
