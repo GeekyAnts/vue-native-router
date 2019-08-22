fb509e907ebb3d009dfcbc37f38c703d776d7c3a
Rishabh Karnad <rishabhk@geekyants.com>
2019-08-22 15:24:50 +0530

    Created example project with TabNavigator usage

---
+++ examples/example-3-tab-navigator-usage/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <app-navigator></app-navigator>
+</template>
+
+<script>
+import {
+  createAppContainer,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from "vue-native-router";
+
+import HomeScreen from "./screens/HomeScreen.vue";
+import DetailsScreen from "./screens/DetailsScreen.vue";
+import SettingsScreen from "./screens/SettingsScreen.vue";
+
+const IOSTabs = createBottomTabNavigator(
+  {
+    Details: DetailsScreen,
+    Settings: SettingsScreen
+  }
+);
+
+const AndroidTabs = createMaterialTopTabNavigator(
+  {
+    Details: DetailsScreen,
+    Settings: SettingsScreen
+  }
+);
+
+const StackNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    AndroidTabs,
+    IOSTabs
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator }
+}
+</script>
